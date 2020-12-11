da71d724cdd23423663c84cfcd5082451023e1e6
Shmurygin Roman <roman.shmurygin@utair.ru>
2020-12-11 19:43:49 +0500

    Курс JS. ДЗ 1.

---
+++ course_js/home_1/script.js
@@ -0,0 +1,17 @@
+'use strict';
+// Задание 1
+let c = parseInt(prompt("Введите температуру в градусах по Цельсию"));
+let f = (9 / 5) * c + 32;
+alert("Температура в градусах по Фаренгейту = " + f);
+
+// Задание 2
+let admin
+let name
+name = prompt("Введите имя");
+admin = name
+alert("Ваше имя: " + admin);
+
+// Задание 3
+const a = 1000
+const b = '108'
+alert("Выражение 1000 + '108' = " + typeof (a + b) + " " + (a + b));
