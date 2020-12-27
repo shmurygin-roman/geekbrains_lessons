6801bbcbc8f465af19ef9bc362d9643f516d1c25
Shmurygin Roman <roman.shmurygin@utair.ru>
2020-12-27 14:52:34 +0500

    Курс JS. ДЗ 4. Правки.

--- course_js/home_4/script.js
+++ course_js/home_4/script.js
@@ -1,44 +1,41 @@
 'use strict';
 // Задание 1
 
+function getObjByNum(num) {
+    if (!Number.isInteger(num) || num < 0 || num > 999) {
+        console.log('Число должно быть от 0 до 999.');
+        return {};
+    }
+
+    const obj = {};
+    obj.единицы = num % 10;
+    obj.десятки = Math.floor(num / 10) % 10;
+    obj.сотни = Math.floor(num / 100);
+    return obj;
+}
+
+console.log(getObjByNum(368));
 
 // Задание 2
 
-class Product {
-    id_product = null;
-    product_name = null;
-    price = null;
-    quantity = null;
-
-    init(_id_product, _product_name, _price, _quantity) {
-        this.id_product = _id_product;
-        this.product_name = _product_name;
-        this.price = _price;
-        this.quantity = _quantity;
+const Basket = {
+    products: [
+        {
+            id_product: 1,
+            product_name: "item_1_name",
+            price: 200,
+            quantity: 5
+        },
+        {
+            id_product: 2,
+            product_name: "item_2_name",
+            price: 500,
+            quantity: 2
+        }
+    ],
+    countBasketPrice() {
+        return this.products.reduce((totalPrice, cartItem) => totalPrice += cartItem.price * cartItem.quantity, 0);
     }
 };
 
-class Basket {
-    products = [];
-
-    addBasket(product) {
-        this.products.push(product);
-    };
-
-    countBasketPrice() {
-        return this.products.reduce((totalPrice, cartItem) => totalPrice += cartItem.price * cartItem.quantity, 0);
-    };
-};
-
-let item_1 = new Product();
-item_1.init(1, 'item_1_name', 200, 5);
-let item_2 = new Product();
-item_2.init(2, 'item_2_name', 500, 2);
-
-let basket = new Basket();
-basket.addBasket(item_1);
-basket.addBasket(item_2);
-
-console.log(basket.countBasketPrice());
-
-
+console.log(Basket.countBasketPrice());
