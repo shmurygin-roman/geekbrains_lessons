70f7472e9803c0487b9b7f7a11a9312aa8d87113
Shmurygin Roman <roman.shmurygin@utair.ru>
2021-09-09 19:50:15 +0500

    Курс Клиент-сервер. Примеры + ДЗ 5.

---
+++ course_client_server/Практическое задание/app_logs/config/config_client_log.py
@@ -0,0 +1,26 @@
+"""Конфиг логгера для клиента"""
+
+import logging.handlers
+import os
+from pathlib import Path
+
+FORMATTER = logging.Formatter("%(asctime)s - %(levelname)-8s - %(message)s ")
+
+FULL_PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = os.path.join(str(Path(FULL_PATH).parents[0]), 'logs\client.log')
+
+FILE_HANDLER = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf8', interval=1, when='midnight')
+FILE_HANDLER.setFormatter(FORMATTER)
+
+
+LOG = logging.getLogger('client')
+LOG.addHandler(FILE_HANDLER)
+LOG.setLevel(logging.DEBUG)
+
+
+if __name__ == '__main__':
+    LOG.debug('Отладочная информация')
+    LOG.info('Информационное сообщение')
+    LOG.warning('Предупреждение')
+    LOG.critical('Критическое общение')
+    LOG.error('Ошибка')
